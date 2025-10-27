e9199c5f12386e8c3915782ce5d36e373617eb3b
philcodee <pcote1@gmail.com>
2025-10-26 21:17:35 -0400

    Create assignment folder

---
+++ Week 7/mouse-events.js
@@ -0,0 +1,3 @@
+// Choose an element of interactivity to make a sketch, drawing or game! 
+// You can use Keyboard, Mouse, Touch or other methods we discussed in class. 
+// If you get stuck for ideas you can try remaking your interactive python game in p5 or review our class notes from week 6.
