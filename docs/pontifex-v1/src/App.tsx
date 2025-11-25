510d18d3f42a05266995788cb1dea33155ad47b4
philcodee <pcote1@gmail.com>
2025-11-24 19:24:18 -0500

    update with build folder

--- docs/pontifex-v1/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Terminal } from "./components/Terminal";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-black flex items-center justify-center p-4">
-      <Terminal />
-    </div>
-  );
-}
